1dd58b40c77aa2b6848bf6d1d6c740a1a4dbefad
blue Deep <deepst0101@gmail.com>
2021-03-28 14:45:42 +0800

    初次提交组件库学习之公共库的代码

---
+++ common/src/main/java/com/blue/common/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.blue.common;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Created by blue on 2021/03/28.
+ * (*^▽^*)
+ * 描述:
+ */
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
